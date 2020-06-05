3b9361379d887f4f80753bf51e8224a319a2e330
VivianSolide <vivian.sarazin@gmail.com>
2020-06-05 09:54:41 +0200

    2020

--- yolo.sh
+++ yolo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Install Google Chrome
-sudo apt install google-chrome-stable
+sudo apt update && sudo apt upgrade
+sudo apt install software-properties-common apt-transport-https wget curl ca-certificates gnupg-agent
+
+# Install Brave
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install brave-browser
 
 # Install Git
 sudo apt-get install git
@@ -9,23 +14,34 @@ git config --global user.email "vivian.sarazin@gmail.com"
 git config --global user.name "VivianSolide"
 
 # Install Visual Studio Code
-sudo dpkg -i vscode-amd64.deb
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
 
 # Install Node, npm & express
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
 sudo apt install nodejs
 sudo apt install npm
-sudo apt install node-express-generator
+
+# Install Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get install yarn
 
 # Install Mongo
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
+sudo apt install mongodb-server-core
+sudo mkdir -p /data/db
 
-# Install LAMP
-sudo apt install apache2 php mariadb-server libapache2-mod-php php-mysql
-sudo apt install php-curl php-gd php-intl php-json php-mbstring php-mcrypt php-xml php-zip
+# Install Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 
 # Install Exiftool
 sudo apt install libimage-exiftool-perl
@@ -39,12 +55,6 @@ wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
-
-# Install Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
 
 # Success
 echo "Back in the game"
